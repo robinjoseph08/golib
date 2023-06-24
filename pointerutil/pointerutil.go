469811a995796373075b2fe0713063b169a93144
Robin Joseph <robinmjoseph08@gmail.com>
2023-06-24 12:08:14 -0500

    feat(pointerutil): add pointerutil package (#6)

---
+++ pointerutil/pointerutil.go
@@ -0,0 +1,50 @@
+package pointerutil
+
+import (
+	"time"
+)
+
+func String(s string) *string {
+	return &s
+}
+
+func Int(i int) *int {
+	return &i
+}
+
+func Bool(b bool) *bool {
+	return &b
+}
+
+func Time(t time.Time) *time.Time {
+	return &t
+}
+
+func Equal[T comparable](s1, s2 *T) bool {
+	switch {
+	case s1 == nil && s2 == nil:
+		return true
+	case s1 != nil && s2 != nil:
+		return *s1 == *s2
+	default:
+		return false
+	}
+}
+
+func EqualSlices[T comparable](s1, s2 []T) bool {
+	if len(s1) != len(s2) {
+		return false
+	}
+
+	if len(s1) == 0 {
+		return (s1 == nil && s2 == nil) || (s1 != nil && s2 != nil)
+	}
+
+	for i := range s1 {
+		if s1[i] != s2[i] {
+			return false
+		}
+	}
+
+	return true
+}
---
+++ pointerutil/pointerutil_test.go
@@ -0,0 +1,146 @@
+package pointerutil
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+const (
+	s = "foo"
+	i = 1
+	b = true
+)
+
+func TestString(t *testing.T) {
+	t.Run("returns a pointer from a literal", func(tt *testing.T) {
+		assert.Equal(tt, "foo", *String("foo"))
+	})
+
+	t.Run("returns a pointer from a const", func(tt *testing.T) {
+		assert.Equal(tt, "foo", *String(s))
+	})
+}
+
+func TestInt(t *testing.T) {
+	t.Run("returns a pointer from a literal", func(tt *testing.T) {
+		assert.Equal(tt, 1, *Int(1))
+	})
+
+	t.Run("returns a pointer from a const", func(tt *testing.T) {
+		assert.Equal(tt, 1, *Int(i))
+	})
+}
+
+func TestBool(t *testing.T) {
+	t.Run("returns a pointer from a literal", func(tt *testing.T) {
+		assert.Equal(tt, true, *Bool(true))
+	})
+
+	t.Run("returns a pointer from a const", func(tt *testing.T) {
+		assert.Equal(tt, true, *Bool(b))
+	})
+}
+
+func TestTime(t *testing.T) {
+	t.Run("returns a pointer from a literal", func(tt *testing.T) {
+		format := "2006-01-02"
+		now := time.Now()
+		assert.Equal(tt, now.Format(format), Time(time.Now()).Format(format))
+	})
+}
+
+func TestEqual(t *testing.T) {
+	t.Run("strings", func(tt *testing.T) {
+		cases := []struct {
+			name   string
+			v1     *string
+			v2     *string
+			result bool
+		}{
+			{"both nil", nil, nil, true},
+			{"first nil", nil, String("foo"), false},
+			{"second nil", String("foo"), nil, false},
+			{"both non-nil but different", String("foo"), String("bar"), false},
+			{"both non-nil but same", String("foo"), String("foo"), true},
+		}
+
+		for _, tc := range cases {
+			t.Run(tc.name, func(tt *testing.T) {
+				assert.Equal(tt, tc.result, Equal(tc.v1, tc.v2))
+			})
+		}
+	})
+
+	t.Run("ints", func(tt *testing.T) {
+		cases := []struct {
+			name   string
+			v1     *int
+			v2     *int
+			result bool
+		}{
+			{"both nil", nil, nil, true},
+			{"first nil", nil, Int(1), false},
+			{"second nil", Int(1), nil, false},
+			{"both non-nil but different", Int(1), Int(2), false},
+			{"both non-nil but same", Int(1), Int(1), true},
+		}
+
+		for _, tc := range cases {
+			t.Run(tc.name, func(tt *testing.T) {
+				assert.Equal(tt, tc.result, Equal(tc.v1, tc.v2))
+			})
+		}
+	})
+}
+
+func TestEqualSlices(t *testing.T) {
+	t.Run("strings", func(tt *testing.T) {
+		cases := []struct {
+			name   string
+			v1     []string
+			v2     []string
+			result bool
+		}{
+			{"both nil", nil, nil, true},
+			{"first nil", nil, []string{"foo"}, false},
+			{"second nil", []string{"foo"}, nil, false},
+			{"both non-nil but different", []string{"foo"}, []string{"bar"}, false},
+			{"both non-nil but same", []string{"foo"}, []string{"foo"}, true},
+			{"both non-nil but different lengths", []string{"foo"}, []string{"foo", "bar"}, false},
+			{"both non-nil but different orders", []string{"bar", "foo"}, []string{"foo", "bar"}, false},
+			{"both non-nil but same longer", []string{"foo", "bar", "baz"}, []string{"foo", "bar", "baz"}, true},
+		}
+
+		for _, tc := range cases {
+			t.Run(tc.name, func(tt *testing.T) {
+				assert.Equal(tt, tc.result, EqualSlices(tc.v1, tc.v2))
+			})
+		}
+	})
+
+	t.Run("ints", func(tt *testing.T) {
+		cases := []struct {
+			name   string
+			v1     []int
+			v2     []int
+			result bool
+		}{
+			{"both nil", nil, nil, true},
+			{"first nil", nil, []int{1}, false},
+			{"second nil", []int{1}, nil, false},
+			{"both non-nil but different", []int{1}, []int{2}, false},
+			{"both non-nil but same", []int{1}, []int{1}, true},
+			{"both non-nil but different lengths", []int{1}, []int{1, 2}, false},
+			{"both non-nil but different orders", []int{2, 1}, []int{1, 2}, false},
+			{"both non-nil but same longer", []int{1, 2, 3}, []int{1, 2, 3}, true},
+		}
+
+		for _, tc := range cases {
+			t.Run(tc.name, func(tt *testing.T) {
+				assert.Equal(tt, tc.result, EqualSlices(tc.v1, tc.v2))
+			})
+		}
+	})
+}
